55fc81dd976f915cfeba38fed901c47873ecf2d7
MWIGAI-JAVACODE <mwigjentrix@gmail.com>
2025-09-20 22:59:27 +0300

    Add files via upload

---
+++ Discount.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter amount: ");
+        double amount = input.nextDouble();
+
+        double discount;
+
+        if (amount > 5000) {
+            discount = amount * 0.10; // 10%
+        } else if (amount > 1000 && amount <= 5000) {
+            discount = amount * 0.05; // 5%
+        } else {
+            discount = 0; // no discount
+        }
+
+        double finalAmount = amount - discount;
+
+        System.out.println("Amount before discount: " + amount);
+        System.out.println("Discount: " + discount);
+        System.out.println("Amount after discount: " + finalAmount);
+    }
+}
